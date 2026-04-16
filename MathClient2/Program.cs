0bc80b6052a60586b702724545c654e9ea750600
Usuario <Usuario@B6-501-26>
2026-04-16 17:18:34 -0500

    Agregar archivos de proyecto.

---
+++ MathClient2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClient2.MathService;
+
+
+namespace MathClient2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MathService.MathServiceSoapClient myMathService = new MathService.MathServiceSoapClient();
+            Console.Write("2 + 4 = {0}", myMathService.Add(2, 4));
+            Console.Write("20 / 2 = {0}", myMathService.Divide(20, 2));
+            Console.Write("2 * 2 = {0}", myMathService.Multiply(2, 2));
+            Console.Write("20 - 2 = {0}", myMathService.Subtract(20, 2));
+
+        }
+
+    }
+}
